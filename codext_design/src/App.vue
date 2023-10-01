7c1535866d9fc45bff09a941ee5f3e69049e5f83
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-10-01 18:00:12 +0545

    codext_design

---
+++ codext_design/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import frontpage from './pages/frontpage.vue'
+</script>
+
+<template>
+ <frontpage/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ codext_design/src/pages/frontpage.vue
@@ -0,0 +1,240 @@
+<template>
+    <div class="container">
+        <div class="content-2">
+            <div class="box1">
+                <img src="public/assets/Group (3).png" style="    width: 100%;height: 100%;">
+            </div>
+            <div class="box1">
+                <div style="margin-left: 10%;">
+
+                 <h1 style="font-size:45px;">It's helpful for operating system</h1>
+                <div style="display:flex; align-items: center;">
+                    <img src="public/assets/Vector (11).png">
+                    <div>
+                     <h3 class="heading1">Ultra fast and secure</h3>
+                    <p class="para">Lorem ipsum dolor sit amet,consectetur adipiscing elit. vitae velit proin justo,elementum siln morbi</p>
+                    </div>
+                    
+                </div>
+
+                <div style="display:flex; align-items: center;">
+                    <img src="public/assets/Vector (12).png">
+                    <div>
+                     <h3 class="heading1">Allows customization</h3>
+                    <p class="para">Lorem ipsum dolor sit amet,consectetur adipiscing elit. vitae velit proin justo,elementum siln morbi</p>
+                    </div>
+                    
+                </div>
+
+                <div style="display:flex; align-items: center;">
+                    <img src="public/assets/Vector (13).png">
+                    <div>
+                     <h3 class="heading1">Smart contract</h3>
+                    <p class="para">Lorem ipsum dolor sit amet,consectetur adipiscing elit. vitae velit proin justo,elementum siln morbi</p>
+                    </div>
+                    
+                </div>
+
+                 </div>
+            </div>
+        </div>
+
+        <div class="content-3">
+            <div class="heading2">
+                <h1 style="font-size: 50px;    width: 53%;">We Provides best Feature for customer</h1>
+            </div>
+
+            <div style="display:flex;justify-content: space-between;">
+            <div class="box2">
+                <div >  
+                        <img src="public/assets/Group 388.png" > 
+                </div>
+                
+                <div style="background:#6023B6;">
+                    <h3>Clean Code</h3>
+                    <p>Lorem ipsum dolor sit amet, ecte  adipiscing elitlpsum </p>
+                    <img src="public/assets/Group 10.png">
+                </div>
+               
+            </div>
+            <div class="box2">
+                <div>
+                    <img src="public/assets/Group 392.png">
+                </div>
+                
+                <div>
+                    <h3>Data Analytic</h3>
+                    <p>Lorem ipsum dolor sit amet, ecte  adipiscing elitlpsum </p>
+                    <img src="public/assets/Group 10.png">
+                </div>
+            </div>
+            <div class="box2">
+                <div>
+                    <img src="public/assets/Group 393.png">
+                </div>
+                
+                <div>
+                    <h3>Fully Secured</h3>
+                    <p>Lorem ipsum dolor sit amet, ecte  adipiscing elitlpsum </p>
+                    <img src="public/assets/Group 10.png">
+                </div>
+            </div>
+            <div class="box2">
+                <div>
+                    <img src="public/assets/Group 394.png">
+                </div>
+                
+                <div>
+                    <h3>24/7 Support </h3>
+                    <p>Lorem ipsum dolor sit amet, ecte  adipiscing elitlpsum </p>
+                    <img src="public/assets/Group 10.png">
+                </div>
+            </div>
+            </div>
+            
+        </div>
+
+        <div class="content-4">
+            <div class="heading3">
+                <h1 style="font-size: 50px;">Choose your best pricing plan</h1>
+                <p style="font-size:25px;margin:0;">Added for monthly /Yearly</p>
+            </div>
+
+
+            <div style="display:flex;justify-content: space-between;">
+            <div class="box3">
+                <p style="font-size: 25px;">Startup</p>
+                <img src="public/assets/Line 2 (1).png" style="width:60%;">
+                <h1>$10/mo</h1>
+                <p>Easy Customizable</p>
+                <p>Commercial Lisence</p>
+                <p>Single user Lisence</p>
+                <p>Hotline support 24/7</p>
+                <button style="border:none;padding:15px;color:#42B4EE;background:#EDF8FE">TRY THIS PACKAGE</button>
+            </div>
+            <div class="box3">
+                <p style="font-size: 25px;">Classic</p>
+                <img src="public/assets/Line 2 (1).png" style="width:60%;">
+                <h1>$30/mo</h1>
+                <p>Easy Customizable</p>
+                <p>Commercial Lisence</p>
+                <p>5 user Lisence</p>
+                <p>Hotline support 24/7</p>
+                <button style="border:none;padding:15px;color:white;background: #42B4EE">TRY THIS PACKAGE</button>
+            </div>
+            <div class="box3">
+                <p style="font-size: 25px;">Premium</p>
+                <img src="public/assets/Line 2 (1).png" style="width:60%;">
+                <h1>$49/mo</h1>
+                <p>Easy Customizable</p>
+                <p>Commercial Lisence</p>
+                <p>10 user Lisence</p>
+                <p>Hotline support 24/7</p>
+                <button style="border:none;padding:15px;color:#42B4EE;background:#EDF8FE">TRY THIS PACKAGE</button>
+            </div>
+            </div>
+            
+        </div>
+
+        <div class="content-5">
+            <div class="heading3">
+                <h1 style="font-size: 50px;">Choose your best pricing plan</h1>
+                
+                    <p>Lorem ipsum dolor sit amet, ecte  adipiscing elit.Nulla scelerisque tellus interdum venenetis auctor et nibh.Rohncus a,sed lobortis nisi. </p>
+                  
+            </div>
+            <div style="display:flex;justify-content: space-between;">
+            <div class="box4"></div>
+            <div class="box4"></div>
+            <div class="box4"></div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.content-2{
+    display:flex;
+    margin-left:10%;
+    margin-right:10%;
+    background:#F7F7F8;
+}
+
+.box1{
+   
+    width:50%;
+}
+
+.para{
+    color:grey;
+    width:75%;
+}
+
+heading1{
+    font-size:20px;
+}
+
+.content-3{
+    margin-left:10%;
+    margin-right:10%;
+    background:#F7F7F8
+}
+
+.heading2{
+    display: flex;
+    justify-content: center;
+   text-align: center;
+}
+
+.box2{
+    background:white;
+    border-radius:20px;
+    width: 24%;
+    display:flex;
+    flex-direction: column;
+    text-align: center;
+}
+
+.content-4{
+    margin-left:10%;
+    margin-right:10%;
+    background:#F7F7F8
+}
+.heading3{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+   text-align: center;
+}
+
+.box3{
+   
+   
+    background:white;
+    border-radius:20px;
+    width: 32%;
+    display:flex;
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+}
+
+.content-5{
+    margin-left:10%;
+    margin-right:10%;
+    background:#F7F7F8
+}
+
+.box4{
+    border:1px solid#C2ACE1;
+    background:white;
+    border-radius:20px;
+    width: 32%;
+    display:flex;
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+
+}
+</style>
